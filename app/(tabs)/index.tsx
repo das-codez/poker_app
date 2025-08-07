055ce2377385ebdb44c00094caedd0113cc755d2
Danyal Saeed <cherrylotter@gmail.com>
2025-08-06 20:52:30 -0500

    main/added-styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,15 +4,24 @@ import {useQuery} from "convex/react"
 import { api } from "@/convex/_generated/api";
 import PlayerInput from "@/components/PlayerInput";
 import { SafeAreaView } from "react-native-safe-area-context";
+import useTheme from "@/hooks/useTheme";
+import {createHomeStyles} from "@/assets/styles/home.styles";
+import {LinearGradient} from "expo-linear-gradient"
+
 export default function Index() {
  const players = useQuery(api.players.getPlayer);
+ const {colors} = useTheme();
+ const homeStyles = createHomeStyles(colors);
  
   return (
-    <SafeAreaView>
+    <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
+
+    <SafeAreaView style={homeStyles.safeArea}>
       <Header/>
       <PlayerInput/>
 
     </SafeAreaView>
+    </LinearGradient>
 
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import { ConvexProvider, ConvexReactClient} from "convex/react";
+import {ThemeProvider} from "@/hooks/useTheme";
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
@@ -8,11 +9,14 @@ const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
 export default function RootLayout() {
   return(
   <ConvexProvider client={convex}>
+    <ThemeProvider>
+
     
       <Stack screenOptions={{headerShown: false}}>
         <Stack.Screen name="(tabs)"/>
       </Stack>
 
+    </ThemeProvider>
   </ConvexProvider>
   );
   
